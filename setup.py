fcf2b1bf5186eeef5f82da6df11a4e077c62a765
Austin <legotown@aol.com>
2013-11-03 18:04:00 -0800

    0.0.4

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from distutils.core import setup, Extension
 python_cec = Extension('cec', sources = [ 'cec.cpp', 'device.cpp' ], 
                         libraries = [ 'cec' ])
 
-setup(name='cec', version='0.0.3',
+setup(name='cec', version='0.0.4',
       description="Python bindings for libcec",
       ext_modules=[python_cec])
